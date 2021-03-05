e9b02025b9046ac63d1eb713ff74fe822a34d910
Kevin <kevin.scott.98@gmail.com>
2021-03-05 22:14:21 +0100

    Tests for broadcasting add operation on gpu

--- test_gpu.py
+++ test_gpu.py
@@ -26,3 +26,47 @@ def test_sum_reduction_kernel():
 def test_sum_all_axes():
     a = np.random.randn(5,10,20,25).astype(np.float32)
     assert equal_sum_over_all_axes(a)
+
+def test_broadcasting_add():
+    a = np.array([[ 0.0,  0.0,  0.0],
+               [10.0, 10.0, 10.0],
+               [20.0, 20.0, 20.0],
+               [30.0, 30.0, 30.0]])
+
+    b = np.array([1.0, 2.0, 3.0])
+
+    tensor_p = (torch.tensor(a)).add(torch.tensor(b))
+    tensor = Tensor(a).add(Tensor(b))
+    assert np.allclose(tensor.val.get(), tensor_p.data.numpy())
+
+    a = np.array([0.0, 10.0, 20.0, 30.0])
+    a = a[:, np.newaxis]
+    b = np.array([1.0, 2.0, 3.0])
+
+    tensor_p = (torch.tensor(a)).add(torch.tensor(b))
+    tensor = Tensor(a).add(Tensor(b))
+    assert np.allclose(tensor.val.get(), tensor_p.data.numpy())
+
+    a = np.random.randn(5,5,5)
+    b = np.random.randn(5,5)
+    tensor_p = (torch.tensor(a)).add(torch.tensor(b))
+    tensor = Tensor(a).add(Tensor(b))
+    assert np.allclose(tensor.val.get(), tensor_p.data.numpy())
+
+    a = np.random.randn(5,5)
+    b = np.random.randn(5,5,5)
+    tensor_p = (torch.tensor(a)).add(torch.tensor(b))
+    tensor = Tensor(a).add(Tensor(b))
+    assert np.allclose(tensor.val.get(), tensor_p.data.numpy())
+
+    a = np.random.randn(3, 1, 2)
+    b = np.random.randn(3, 1)
+    tensor_p = (torch.tensor(a)).add(torch.tensor(b))
+    tensor = Tensor(a).add(Tensor(b))
+    assert np.allclose(tensor.val.get(), tensor_p.data.numpy())
+
+    a = np.random.randn(3, 4)
+    b = np.random.randn(2, 3, 4)
+    tensor_p = (torch.tensor(a)).add(torch.tensor(b))
+    tensor = Tensor(a).add(Tensor(b))
+    assert np.allclose(tensor.val.get(), tensor_p.data.numpy())
