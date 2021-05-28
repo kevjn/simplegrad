ab7408a70ee0b5cc26752e352d325cd50bfd57f2
Kevin <kevin.scott.98@gmail.com>
2021-05-28 23:13:06 +0200

    GPU tests

--- test/test_gpu.py
+++ test/test_gpu.py
@@ -3,12 +3,13 @@ from simplegrad import Device, Tensor
 import torch
 
 np.random.seed(1337)
-Tensor.device = Device.GPU()
+Device.GPU()
+Tensor.device.array = Device.GPU.array
 
 def equal_sum_over_axis(arr, *, axis):
     res_cpu = np.sum(arr, axis=axis)
-    res_gpu = Tensor(arr).sum(axis=axis).data
-    return np.allclose(res_cpu, res_gpu.get(), rtol=1e-04, atol=1e-07)
+    res_gpu = Tensor(Device.GPU.array(arr)).sum(axis=axis).data
+    return np.allclose(res_cpu, res_gpu.get(), rtol=1e-04, atol=1e-05)
 
 def equal_sum_over_all_axes(arr):
     res_cpu = np.sum(arr)
@@ -20,24 +21,25 @@ def equal_sum_over_all_axes(arr):
 
 def equal_add(a: np.ndarray, b: np.ndarray):
     tensor_pytorch = torch.tensor(a).add(torch.tensor(b))
-    tensor_simplegrad = Tensor(a).add(Tensor(b))
+    tensor_simplegrad = Tensor(Device.GPU.array(a)).add(Tensor(Device.GPU.array(b)))
     return np.allclose(tensor_simplegrad.data.get(), tensor_pytorch.data.numpy(), atol=1e-07)
 
 def equal_einsum(subscripts, a: np.ndarray, b: np.ndarray):
     res_cpu = np.einsum(subscripts, a, b)
-    res_gpu = Tensor(a).einsum(subscripts, Tensor(b)).data
+    res_gpu = Tensor(Device.GPU.array(a, dtype=np.float32))\
+                .einsum(subscripts, Tensor(Device.GPU.array(b, dtype=np.float32))).data
     return np.allclose(res_cpu, res_gpu.get(), rtol=1e-04, atol=1e-06)
 
 def test_sum_reduction_kernel():
-    a = np.random.randn(10,100,60,5).astype(np.float32)
+    a = np.random.randn(5,5).astype(np.float32)
 
     for i in range(a.ndim):
         assert equal_sum_over_axis(a, axis=i)
 
 def test_sum_all_axes():
-    a = np.random.randn(10,100,200,255).astype(np.float32)
+    a = np.random.randn(10,10,20,25).astype(np.float32)
 
-    res_gpu = Tensor(a).sum(axis=(0,1,2,3)).data
+    res_gpu = Tensor(Device.GPU.array(a)).sum(axis=(0,1,2,3)).data
     res_cpu = np.sum(a, axis=None)
     assert np.allclose(res_cpu, res_gpu.get(), rtol=1e-04, atol=1e-07)
 
@@ -56,7 +58,7 @@ def test_sum_all_axes_at_once():
     a = np.random.randn(5,25,10,30).astype(np.float32)
 
     res_cpu = np.sum(a, axis=None)
-    res_gpu = Tensor(a).sum(axis=None).data
+    res_gpu = Tensor(Device.GPU.array(a)).sum(axis=None).data
 
     assert np.allclose(res_cpu, res_gpu.get(), rtol=1e-04, atol=1e-07)
 
@@ -112,12 +114,12 @@ def test_einsum_reduction_with_unequal_ndim():
     w = np.random.randn(2, 64)
     assert equal_einsum("ijk,kl->ijl", k, w)
 
-    # assert equal_einsum("jki,ji->ik", a, b) # TODO: fix this
-    # assert equal_einsum("ijk,jl->ik", a, b) # TODO: fix this
+    assert equal_einsum("jki,ji->ik", a, b)
+    assert equal_einsum("ijk,jl->ik", a, b)
 
 def test_einsum_with_no_reduction():
-    a = np.random.randn(10,10).astype(np.float32)
-    b = np.random.randn(10,10).astype(np.float32)
+    a = np.random.randn(5,5).astype(np.float32)
+    b = np.random.randn(5,5).astype(np.float32)
 
     # Matrix hadamard product
     assert equal_einsum("ij,ij->ij", a, b)
@@ -131,6 +133,44 @@ def test_einsum_with_no_reduction():
     assert equal_einsum("ji,ji->ij", a, b)
 
     assert equal_einsum("ij,ij->ji", a, b)
+
+def test_einsum_advanced():
+    a = np.random.randn(10, 16, 18, 38, 3, 3).astype(np.float32)
+    b = np.random.randn(20, 16, 3, 3).astype(np.float32)
+
+    # conv2d
+    subscripts = "abcdef,gbef->agcd"
+
+    res_gpu = Tensor(Device.GPU.array(a)).einsum(subscripts, Tensor(Device.GPU.array(b))).data
+    res_cpu = np.einsum(subscripts, a, b)
+
+    assert np.allclose(res_cpu, res_gpu.get(), rtol=1e-04, atol=1e-05)
+
+
+def test_transpose():
+    a = np.random.randn(1,2,3,4).astype(np.float32)
+    b = Device.GPU.array(a)
+
+    a = a.transpose(0,1,2,3)
+    b = b.transpose(0,1,2,3)
+    np.array_equal(a, b.get())
+
+    a = a.transpose(2,1,0,3)
+    b = b.transpose(2,1,0,3)
+    np.array_equal(a, b.get())
+
+    a = a.transpose(1,0,2,3)
+    b = b.transpose(1,0,2,3)
+    np.array_equal(a, b.get())
+
+    a = a.transpose(1,2,0,3)
+    b = b.transpose(1,2,0,3)
+    np.array_equal(a, b.get())
+
+    a = a.transpose(0,2,1,3)
+    b = b.transpose(0,2,1,3)
+    np.array_equal(a, b.get())
+
 
 def test_broadcasting_add():
     a = np.array([[ 0.0,  0.0,  0.0],
@@ -179,12 +219,23 @@ def test_simple_backward_with_broadcasting():
 
     tensor_p = (t_p := torch.tensor(a, requires_grad=True)).add(w_p := torch.tensor(w, requires_grad=True)).relu().sum(axis=0).sum(axis=0).sum(axis=0)
     tensor_p.backward()
-    tensor = Tensor(a).add(w_s := Tensor(w)).relu().sum(axis=0).sum(axis=0).sum(axis=0)
+
+    tensor = Tensor(Device.GPU.array(a)).add(w_s := Tensor(Device.GPU.array(w))).relu().sum(axis=0).sum(axis=0).sum(axis=0)
     tensor.backward()
 
     assert np.allclose(tensor.data.get(), tensor_p.data.numpy())
     assert np.allclose(tensor.grad.get(), t_p.grad.data.numpy())
     assert np.allclose(w_s.grad.get(), w_p.grad.data.numpy())
+
+def test_sum_backward():
+    a = np.random.randn(10,10)
+    tensor_p = (tp := torch.tensor(a, requires_grad=True)).sum(axis=0).sum(axis=0)
+    tensor_p.backward()
+    ts = Tensor(Device.GPU.array(a)).sum(axis=0).sum(axis=0)
+    ts.backward()
+
+    assert np.allclose(ts.data.get(), tensor_p.data.numpy())
+    assert np.allclose(ts.grad.get(), tp.grad.data.numpy())
 
 def test_pow_backward_pass():
     a = np.random.randn(10,10)
@@ -192,10 +243,31 @@ def test_pow_backward_pass():
     tp = torch.tensor(a, requires_grad=True)
     tp.pow(torch.tensor(2)).sum().backward()
 
-    ts = Tensor(a).pow(Tensor(2))
+    ts = Tensor(Device.GPU.array(a)).pow(2).sum()
     ts.backward()
 
     np.allclose(tp.grad.data.numpy(), ts.grad.get())
+
+def test_mean_backward():
+    a = np.random.randn(10,10,1)
+    ts = Tensor(Device.GPU.array(a)).mean()
+    ts.backward()
+
+def test_relu_backward():
+    a = np.random.randn(10,10,1)
+    w = np.random.randn(1,10,5)
+
+    tp = torch.tensor(a, requires_grad=True)
+    wp = torch.tensor(w, requires_grad=True)
+    tp.mul(wp).relu().mean().backward()
+
+    ws = Tensor(Device.GPU.array(w))
+    ts = Tensor(Device.GPU.array(a)).mul(ws).relu().mean()
+    ts.backward()
+
+    assert np.allclose(tp.grad.data.numpy(), ts.grad.get())
+    assert np.allclose(wp.grad.data.numpy(), ws.grad.get())
+
 
 def test_forward_and_backward_for_simple_classifier():
     X = np.random.randn(3000, 2)
@@ -209,7 +281,7 @@ def test_forward_and_backward_for_simple_classifier():
     tp = torch.tensor(X, requires_grad=True)
     outp = tp.matmul(w0p := torch.tensor(w0, requires_grad=True)).add(torch.tensor(b0))\
         .relu().matmul(w1p := torch.tensor(w1, requires_grad=True)).add(torch.tensor(b1))
-    
+
     outp = torch.functional.F.log_softmax(outp)
 
     # Negative LL loss
@@ -217,14 +289,14 @@ def test_forward_and_backward_for_simple_classifier():
     lossp.backward() 
 
     # ========== simplegrad ==========
-    outs = Tensor(X).einsum("ij,jk->ik", w0s := Tensor(w0))\
-        .add(Tensor(b0)).relu().einsum("ij,jk->ik", w1s := Tensor(w1))\
-        .add(Tensor(b1))
+    outs = Tensor(Device.GPU.array(X)).einsum("ij,jk->ik", w0s := Tensor(Device.GPU.array(w0)))\
+        .add(Tensor(Device.GPU.array(b0))).relu().einsum("ij,jk->ik", w1s := Tensor(Device.GPU.array(w1)))\
+        .add(Tensor(Device.GPU.array(b1)))
 
     outs.logsoftmax()
-    
+
     # Negative LL loss
-    loss = Tensor(y).mul(outs).mul(Tensor(-1)).sum(axis=1).mean()
+    loss = Tensor(Device.GPU.array(y)).mul(outs).mul(-1).sum(axis=1).mean()
     loss.backward()
 
     assert np.allclose(outs.data.get(), outp.data.numpy(), rtol=1e-04, atol=1e-05)
@@ -232,7 +304,12 @@ def test_forward_and_backward_for_simple_classifier():
     assert np.allclose(w0s.grad.get(), w0p.grad.data.numpy(), rtol=1e-04, atol=1e-05)
     assert np.allclose(w1s.grad.get(), w1p.grad.data.numpy(), rtol=1e-04, atol=1e-05)
 
-def test_train_simple_classifier():
-    from test.test_tensor import test_train_simple_classifier
-    test_train_simple_classifier()
-    assert True
+# def test_train_simple_classifier():
+#     from test.test_tensor import test_train_simple_classifier
+#     test_train_simple_classifier()
+#     assert True
+
+# def test_cupy():
+#     import cupy as cp
+#     Tensor.device = cp
+#     Tensor.device.Array = cp.array
