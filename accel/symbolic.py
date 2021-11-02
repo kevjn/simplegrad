a223d01c9e4aa2276e7e5ecc22c80ba105471b0a
Kevin Scott <kevin.scott.98@gmail.com>
2021-11-02 09:55:03 +0100

    Use intermediary variables for symbolic expression

--- accel/symbolic.py
+++ accel/symbolic.py
@@ -12,10 +12,17 @@ class SymbolicArray(np.ndarray):
 
         return obj
 
+    def expand(self):
+        sym = self.symbolic
+        for k,v in self.locals.items():
+            sym = sym.replace(k,v)
+        return sym
+
     def __array_finalize__(self, obj) -> None:
         if obj is None: return
         # This attribute should be maintained!
         self.symbolic = getattr(obj, 'symbolic', None)
+        self.locals = getattr(obj, 'locals', {})
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
         assert method == '__call__'
@@ -25,13 +32,27 @@ class SymbolicArray(np.ndarray):
 
     def __array_function__(self, func, types, inputs, kwargs):
         assert func.__module__
-        return SymbolicArray (
+        obj = SymbolicArray (
             func (
                 *(x.view(np.ndarray) if isinstance(x, SymbolicArray) else x for x in inputs),
                 **kwargs
             ),
             f"{func.__module__}.{func.__name__}({', '.join(self._symbolic_args(inputs, kwargs))})"
         )
+
+        for x in filter(lambda x: isinstance(x, type(self)), inputs):
+            obj.locals |= x.locals
+
+            if obj.symbolic.count(x.symbolic) > 1 and len(x.symbolic) > 5:
+                var = f"var{abs(hash(x.symbolic))}"
+
+                # add symbol to memory
+                obj.locals[var] = x.symbolic
+
+                # substitute
+                obj.symbolic = obj.symbolic.replace(x.symbolic, var)
+
+        return obj
 
     def _symbolic_args(self, inputs, kwargs): 
         return it.chain (
